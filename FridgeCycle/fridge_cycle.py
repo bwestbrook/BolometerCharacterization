630a23ca0141030cd9c57960993f9202353f23d2
unknown <bwestbrook@berkeley.edu>
2019-05-06 15:43:10 -0700

    working version of fridge cycle tested

--- FridgeCycle/fridge_cycle.py
+++ FridgeCycle/fridge_cycle.py
@@ -12,9 +12,12 @@ class FridgeCycle():
         self.mm_port = mm_port
         self.mm_connection = lab_serial(port=self.mm_port)
         self.initialize_mm_for_resistance()
-        self.check_resistance()
+        try:
+            self.check_resistance()
+        except ValueError:
+            import ipdb;ipdb.set_trace()
         self.apply_voltage(1)
-        import ipdb;ipdb.set_trace()
+        #import ipdb;ipdb.set_trace()
 
     def _send_ps_command(self, msg):
         self.ps_connection.write(msg)
@@ -47,23 +50,30 @@ class FridgeCycle():
         print(resistance)
         return resistance
 
-    def run_cycle(self, charcoal_start_resistance=5e5, charcoal_end_resistance=3.5e3, sleep_time=0.5):
+    def run_cycle(self, charcoal_start_resistance=1e3, charcoal_end_resistance=0.5e3, sleep_time=0.5):
         global do_cycle_fridge
-        do_fridge_cyle = True
+        do_cycle_fridge = True
         resistance = self.check_resistance()
         # Wait for charcoal to cool down
+        print('Start Cool', resistance, charcoal_start_resistance, do_cycle_fridge)
         while resistance < charcoal_start_resistance and do_cycle_fridge:
             time.sleep(sleep_time)
             resistance = self.check_resistance()
-            print(resistance)
+            print('Cooling', resistance, charcoal_start_resistance, do_cycle_fridge)
+        print('Charcol has reached {0} Turning on Voltage'.format(charcoal_start_resistance))
         # Turn on voltage in steps of 1 volt over with a sleep between
-        for i in range(26):
+        for i in range(3):
             time.sleep(sleep_time)
             self.apply_voltage(i)
+        print('Start Heat', resistance, charcoal_end_resistance, do_cycle_fridge)
+        print('Voltage to heater set to {0} V'.format(i))
         while resistance > charcoal_end_resistance and do_cycle_fridge:
             time.sleep(sleep_time)
             resistance = self.check_resistance()
-            print(resistance)
+            print('Heating', resistance, charcoal_start_resistance, do_cycle_fridge)
+        print('Charcol has reached {0} Turning on Voltage'.format(charcoal_start_resistance))
+        self.apply_voltage(0)
+        print('Voltage on heater set to 0V')
 
     def stop_cycle(self, charcoal_start_resistance=5e5, charcoal_end_resistance=3.5e3, sleep_time=0.5):
         global do_cycle_fridge
--- ba_settings/fridge_cycle.py
+++ ba_settings/fridge_cycle.py
@@ -8,9 +8,9 @@ fridge_cycle_settings.fridge_cycle_popup_build_dict = {
 
                                                        '_fridge_cycle_popup_start_resistance_header_label': {'text': 'Start Resistance', 'position': (1, 0, 1, 1)},
                                                        '_fridge_cycle_popup_start_resistance_header_label': {'text': 'Start Resistance', 'position': (1, 0, 1, 1)},
-                                                       '_fridge_cycle_popup_start_resistance_lineedit': {'text': '1e6', 'position': (1, 1, 1, 1)},
+                                                       '_fridge_cycle_popup_start_resistance_lineedit': {'text': '1e3', 'position': (1, 1, 1, 1)},
                                                        '_fridge_cycle_popup_end_resistance_header_label': {'text': 'End Resistance', 'position': (2, 0, 1, 1)},
-                                                       '_fridge_cycle_popup_end_resistance_lineedit': {'text': '3.5e3', 'position': (2, 1, 1, 1)},
+                                                       '_fridge_cycle_popup_end_resistance_lineedit': {'text': '0.5e3', 'position': (2, 1, 1, 1)},
                                                        '_fridge_cycle_popup_start_cycle_pushbutton': {'text': 'Start Cycle', 'function': '_start_fridge_cycle', 'position': (3, 0, 1, 1)},
                                                        '_fridge_cycle_popup_stop_cycle_pushbutton': {'text': 'Stop Cycle', 'function': '_stop_fridge_cycle', 'position': (3, 1, 1, 1)},
                                                        '_fridge_cycle_popup_close_fridge_cycle_pushbutton': {'text': 'Close', 'function': '_close_fridge_cycle', 'position': (4, 0, 1, 2)},
